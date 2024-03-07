a5b5eb581688b4b4a251a353964aafc13d018347
Kateryna Martyniuk <150445981+KatiaLav@users.noreply.github.com>
2024-03-07 11:17:31 +0200

    Delete lessons_js directory

--- lessons_js/lesson/js/cycle15.js
+++
@@ -1,8 +0,0 @@
-
-let averageTemperature = 0;
-for (let temp = 1; temp <= 12; temp++ ) {
-	const indicator = parseFloat(prompt(`Введіть показник температури за ${temp} місяць`));
-	averageTemperature += indicator;
-	}
-document.write(`Середня температура за рік ${averageTemperature / 12}`);
-
