d581798d6bdd8d8b6b4e6e998f7895bba6f9b308
Kateryna Martyniuk <150445981+KatiaLav@users.noreply.github.com>
2024-05-01 23:01:24 +0300

    Delete lessons_18 directory

--- lessons_18/js/js-3.js
+++
@@ -1,8 +0,0 @@
-"use strict"
-
-function getAgeUser() {
-	const userYear = parseInt(document.getElementById('userYear').value)
-	const nowYear = new Date().getFullYear()
-	document.getElementById('resultAge').value = nowYear - userYear
-}
-document.getElementById('brdAge').onclick = getAgeUser
